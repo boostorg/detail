fbf832c900801cda136a5e1bf8a9f007282b0e43
Robert Ramey <ramey@rrsd.com>
2016-02-26 17:58:54 -0800

    make utf8 visible

--- include/boost/detail/utf8_codecvt_facet.hpp
+++ include/boost/detail/utf8_codecvt_facet.hpp
@@ -106,7 +106,7 @@ BOOST_UTF8_BEGIN_NAMESPACE
 //----------------------------------------------------------------------------//
 
 
-struct  utf8_codecvt_facet :
+struct  BOOST_SYMBOL_VISIBLE utf8_codecvt_facet :
     public std::codecvt<wchar_t, char, std::mbstate_t>  
 {
 public:
